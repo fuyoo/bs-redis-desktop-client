e1ebced59f61daa37860fa0e49904cdd4657f130
fuyoo <fyaiym@qq.com>
2022-06-07 18:37:10 +0800

    add doc

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ extern crate sciter;
 
 use std::io::Write;
 use flume::{Receiver, RecvError};
-use anyhow::{anyhow, Error, Result};
-use logger::info;
+use anyhow::{anyhow, Error, Result};;
+use logger::{info, error};
 use crate::events::{Action, Evt};
 use crate::logger::open_log_file;
 
@@ -43,21 +43,30 @@ fn create_main() -> Result<(), Error> {
         sciter::SCRIPT_RUNTIME_FEATURES::ALLOW_SYSINFO as u8        // Enables `Sciter.machineName()`.  Required for opening file dialog (`view.selectFile()`)
             | sciter::SCRIPT_RUNTIME_FEATURES::ALLOW_FILE_IO as u8    // Enables opening file dialog (`view.selectFile()`)
     )).map_err(|e| anyhow!("{:?}",e))?;
+    // at rust debug module,open the sciter debug mode
     #[cfg(debug_assertions)]
-    sciter::set_options(sciter::RuntimeOptions::DebugMode(true)).map_err(|_|{anyhow!("unknown error")})?;
+    sciter::set_options(sciter::RuntimeOptions::DebugMode(true)).map_err(|_| { anyhow!("unknown error") })?;
+    // include sciter packfolder tool crated the resource file
     let resource = include_bytes!("resource.rc");
+    // create window
     let mut window = sciter::WindowBuilder::main_window()
         .with_size((1024, 600))
         .create();
+    // load resource file
     window.archive_handler(resource).map_err(|e| anyhow!("{:?}",e))?;
+    // generate channel to dispatch data
     let (sender, reciver) = flume::unbounded();
+    // listen channel
     do_request_services(reciver);
+    // inject event
     window.event_handler(Evt { sender });
+    // load ui
     if cfg!(target_os = "windows") {
         window.load_file("this://app/index_win.html");
     } else {
         window.load_file("this://app/index.html");
     }
+    // start event-loop
     window.run_app();
     Ok(())
 }
@@ -65,21 +74,17 @@ fn create_main() -> Result<(), Error> {
 fn do_request_services(receiver: Receiver<Action>) {
     tokio::spawn(async move {
         loop {
-            let res = receiver.recv_async().await;
-            match res {
+            match receiver.recv_async().await {
                 Ok(action) => {
                     match routes::dispatch(action).await {
                         Ok(_) => {}
-                        Err(err) => {
-                            println!("err {}", err);
-                        }
+                        Err(err) => error!("{}", err)
                     }
                 }
-                Err(e) => {
-                    match e {
-                        RecvError::Disconnected => {
-                            break;
-                        }
+                Err(e) => match e {
+                    RecvError::Disconnected => {
+                        error!("do_request_services channel disconnected");
+                        std::process::exit(0);
                     }
                 }
             }
