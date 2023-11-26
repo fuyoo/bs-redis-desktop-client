1ccd4b430cdabaaf769cf3a3276c2b74098ee7c7
fuyoo <fuyoo0928@gmail.com>
2023-11-26 21:44:57 +0800

    remove windows lock, instead of FindWindow api.

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -15,49 +15,61 @@ use tauri::api::path;
 use tokio::{sync::oneshot, task::JoinHandle as TokioJoinHandle};
 #[cfg(target_os = "windows")]
 use windows::{
-    w,
     Win32::{
-        Foundation::{GetLastError, WIN32_ERROR},
+        Foundation::{GetLastError, WIN32_ERROR,HWND},
         Globalization::GetUserDefaultUILanguage,
         System::Threading::{CreateMutexW, OpenMutexW, SYNCHRONIZATION_ACCESS_RIGHTS},
-        UI::WindowsAndMessaging::{MessageBoxW, MB_OK},
+        UI::WindowsAndMessaging::{MessageBoxW, MB_OK,FindWindowW, SetForegroundWindow, ShowWindow, SW_NORMAL},
     },
+    core::w,
 };
 
 /// make sure app running at single-case
 #[cfg(target_os = "windows")]
 pub fn make_sure_single_case() {
     unsafe {
-        let _ = OpenMutexW(
+        let  hwnd  = FindWindowW(None,w!("BS"));
+
+        if hwnd != HWND(0) {
+            std::process::exit(0);
+        } else {
+            ShowWindow(hwnd,SW_NORMAL);
+            SetForegroundWindow(hwnd);
+        }
+       /* let _ = OpenMutexW(
             SYNCHRONIZATION_ACCESS_RIGHTS(0),
             true,
             w!("link.xsa.bs@fuyoo"),
         );
-        let WIN32_ERROR(code) = GetLastError();
-        if code == 2 {
-            // generate lock
-            let _ = CreateMutexW(None, true, w!("link.xsa.bs@fuyoo"));
-        } else {
-            // get local language
-            let lang = GetUserDefaultUILanguage();
-            // Adapt to Chinese and english
-            if lang == 2052u16 {
-                MessageBoxW(
-                    None,
-                    w!("Rd 正在运行中, 请不要重复运行！"),
-                    w!("提示"),
-                    MB_OK,
-                );
-            } else {
-                MessageBoxW(
-                    None,
-                    w!("Rd is Running, Please do not run it again!"),
-                    w!("Tips"),
-                    MB_OK,
-                );
+        let res = GetLastError();
+        match res {
+            Err(_) => {
+                // get local language
+                let lang = GetUserDefaultUILanguage();
+                // Adapt to Chinese and english
+                if lang == 2052u16 {
+                    MessageBoxW(
+                        None,
+                        w!("Rd 正在运行中, 请不要重复运行！"),
+                        w!("提示"),
+                        MB_OK,
+                    );
+                } else {
+                    MessageBoxW(
+                        None,
+                        w!("Rd is Running, Please do not run it again!"),
+                        w!("Tips"),
+                        MB_OK,
+                    );
+                }
+                std::process::exit(0);
             }
-            std::process::exit(0);
-        }
+            Ok(_) => {
+                // generate lock
+                let _ = CreateMutexW(None, true, w!("link.xsa.bs@fuyoo"));
+            }
+        }*/
+
     }
 }
 
