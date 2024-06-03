f317076f348172ba1815b4ae07b5f8c5eee40ee4
fuyoo <fuyoo0928@gmail.com>
2024-06-03 11:10:40 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use tauri::Manager;
+#[cfg(target_os = "windows")]
 use window_shadows::set_shadow;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,8 +12,11 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let window = app.get_window("main").unwrap();
-            set_shadow(&window, true).expect("Unsupported platform!");
+            #[cfg(target_os = "windows")]
+            {
+              let window = app.get_window("main").unwrap();
+              set_shadow(&window, true).expect("Unsupported platform!");  
+            }
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
